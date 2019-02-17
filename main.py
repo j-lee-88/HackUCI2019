04c0b5e9da9ac4b66cc84dd28f9138054478ac45
Justin <justinlee3557@gmail.com>
2019-02-16 21:43:22 -0800

    Update main.py

--- main.py
+++ main.py
@@ -3,3 +3,28 @@ import CSV-parser
 
 def Main():
     
+
+
+
+def getInput():
+    
+    f = open("student_expenditures", "w+")
+    
+    print("Which type of student are you?")
+    print("1) Public Two-Year In-State Commuter")
+    print("2) Public Four-Year In-State On-Campus")
+    print("3) Public Four-Year Out-Of_State On-Campus")
+    print("4) Private Nonprofit Four-Year On-Campus")
+
+    option = input()
+    
+    if (option == 1):
+
+    elif (option == 2):
+
+    elif (option == 3):
+
+    elif (option == 4):
+
+    else:
+        
