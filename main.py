13aa5dfb8f45eddc77d90e813b1822b076446356
Justin <justinlee3557@gmail.com>
2019-02-17 00:03:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ def getInput():
     print("Which type of student are you?")
     print("1) Public Two-Year In-State Commuter")
     print("2) Public Four-Year In-State On-Campus")
-    print("3) Public Four-Year Out-Of_State On-Campus")
+    print("3) Public Four-Year Out-Of State On-Campus")
     print("4) Private Nonprofit Four-Year On-Campus")
 
     option = int(input())
